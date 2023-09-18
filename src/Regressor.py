aac0ae4d8c262f9ab36425bb1828e670a0523b7b
moo3030 <mohamedabdelhamid3030@gmail.com>
2023-09-18 08:15:52 +0300

    init

---
+++ src/Regressor.py
@@ -0,0 +1,174 @@
+import os
+import re
+import warnings
+from typing import List
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn.exceptions import NotFittedError
+from pycaret.regression import setup, compare_models, predict_model, finalize_model
+from schema.data_schema import RegressionSchema
+
+warnings.filterwarnings("ignore")
+
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/configs/"
+
+PREDICTOR_FILE_NAME = "predictor.joblib"
+
+
+def clean_and_ensure_unique(names: List[str]) -> List[str]:
+    """
+    Clean the provided column names by removing special characters and ensure their
+    uniqueness.
+
+    The function first removes any non-alphanumeric character (except underscores)
+    from the names. Then, it ensures the uniqueness of the cleaned names by appending
+    a counter to any duplicates.
+
+    Args:
+        names (List[str]): A list of column names to be cleaned.
+
+    Returns:
+        List[str]: A list of cleaned column names with ensured uniqueness.
+
+    Example:
+        >>> clean_and_ensure_unique(['3P%', '3P', 'Name', 'Age%', 'Age'])
+        ['3P', '3P_1', 'Name', 'Age', 'Age_1']
+    """
+
+    # First, clean the names
+    cleaned_names = [re.sub('[^A-Za-z0-9_]+', '', name) for name in names]
+
+    # Now ensure uniqueness
+    seen = {}
+    for i, name in enumerate(cleaned_names):
+        original_name = name
+        counter = 1
+        while name in seen:
+            name = original_name + "_" + str(counter)
+            counter += 1
+        seen[name] = True
+        cleaned_names[i] = name
+
+    return cleaned_names
+
+
+class Regressor:
+    """A wrapper class for the LightGBM Regressor.
+
+    This class provides a consistent interface that can be used with other
+    regressor models.
+    """
+
+    model_name = "pycaret_regressor_model"
+
+    def __init__(
+            self,
+            train_input: pd.DataFrame,
+            schema: RegressionSchema
+    ):
+        """Construct a new PyCaret Regressor.
+
+        Args:
+           train_input (pd.DataFrame): The input data for model training.
+           schema (RegressionSchema): Schema of the input data.
+        """
+        self._is_trained = False
+        self.train_input = train_input
+        self.schema = schema
+        self.setup(train_input, schema)
+        self.model = self.compare_models()
+
+    def setup(self, train_input: pd.DataFrame, schema: RegressionSchema):
+        """Set up the experiment of comparing different models.
+        Args:
+            train_input: The data  of training including the target column.
+            schema: schema of the provided data.
+        """
+        setup(train_input, target=schema.target, remove_outliers=True, normalize=True, ignore_features=[schema.id])
+
+    def compare_models(self):
+        """Compares multiple regression models."""
+        self._is_trained = True
+        return compare_models()
+
+    def predict(self, inputs: pd.DataFrame) -> np.ndarray:
+        """Predict regression targets for the given data.
+
+        Args:
+            inputs (pandas.DataFrame): The input data.
+        Returns:
+            numpy.ndarray: The predicted regression targets.
+        """
+        return self.model.predict(inputs)
+
+    def save(self, model_dir_path: str) -> None:
+        """Save the regressor model to disk.
+        Args:
+            model_dir_path (str): Dir path to which to save the model.
+        """
+
+        if not self._is_trained:
+            raise NotFittedError("Model is not fitted yet.")
+        pipeline = finalize_model(self.model)
+        joblib.dump(pipeline, os.path.join(model_dir_path, PREDICTOR_FILE_NAME))
+
+    @classmethod
+    def load(cls, model_dir_path: str) -> "Regressor":
+        """Load the regressor from disk.
+
+        Args:
+            model_dir_path (str): Dir path to the saved model.
+        Returns:
+            Regressor: A new instance of the loaded regressor.
+        """
+        model = joblib.load(os.path.join(model_dir_path, PREDICTOR_FILE_NAME))
+        return model
+
+    def __str__(self):
+        # sort params alphabetically for unit test to run successfully
+        return (
+            f"Model name: {type(self.model).__name__}"
+        )
+
+
+def predict_with_model(regressor: "Regressor", data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Predict class probabilities for the given data.
+
+    Args:
+        regressor (Regressor): The regressor model.
+        data (pd.DataFrame): The input data.
+
+    Returns:
+        np.ndarray: The predicted label.
+    """
+    return predict_model(regressor, data)
+
+
+def save_predictor_model(model: Regressor, predictor_dir_path: str) -> None:
+    """
+    Save the regressor model to disk.
+
+    Args:
+        model (Regressor): The regressor model to save.
+        predictor_dir_path (str): Dir path to which to save the model.
+    """
+    if not os.path.exists(predictor_dir_path):
+        os.makedirs(predictor_dir_path)
+    model.save(predictor_dir_path)
+
+
+def load_predictor_model(predictor_dir_path: str) -> Regressor:
+    """
+    Load the regressor model from disk.
+
+    Args:
+        predictor_dir_path (str): Dir path where model is saved.
+
+    Returns:
+        Regressor: A new instance of the loaded regressor model.
+    """
+    return Regressor.load(predictor_dir_path)
+
+
---
+++ src/predict.py
@@ -0,0 +1,39 @@
+from config import paths
+from utils import read_csv_in_directory, save_dataframe_as_csv
+from logger import get_logger
+from Regressor import Regressor, predict_with_model
+from schema.data_schema import load_saved_schema
+
+logger = get_logger(task_name="predict")
+
+
+def run_batch_predictions() -> None:
+    """
+        Run batch predictions on test data, save the predicted probabilities to a CSV file.
+
+        This function reads test data from the specified directory,
+        loads the preprocessing pipeline and pre-trained predictor model,
+        transforms the test data using the pipeline,
+        makes predictions using the trained predictor model,
+        adds ids into the predictions dataframe,
+        and saves the predictions as a CSV file.
+        """
+    x_test = read_csv_in_directory(paths.TEST_DIR)
+    data_schema = load_saved_schema(paths.SAVED_SCHEMA_DIR_PATH)
+    model = Regressor.load(paths.PREDICTOR_DIR_PATH)
+    logger.info("Making predictions...")
+    predictions_df = predict_with_model(model, x_test)[[data_schema.id, 'prediction_label']]
+    predictions_df.rename(columns={'prediction_label': data_schema.target}, inplace=True)
+
+    print(predictions_df)
+
+    logger.info("Saving predictions...")
+    save_dataframe_as_csv(
+        dataframe=predictions_df, file_path=paths.PREDICTIONS_FILE_PATH
+    )
+
+    logger.info("Batch predictions completed successfully")
+
+
+if __name__ == "__main__":
+    run_batch_predictions()
